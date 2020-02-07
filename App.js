bfa29b6a6b127f00088cb0676763d5fe8ff72354
Shrey Jain <shrey.jain711@gmail.com>
2020-02-07 17:51:31 +0530

    finalising prototype #1

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Header from './components/Header';
 import { HomeScreen } from './components/HomeScreen'
-import { DetailsScreen } from './components/DetailsScreen'
+import { ATMScreen } from './components/ATMScreen';
+import { BankScreen } from './components/BankScreen';
+import { BMScreen } from './components/BMScreen';
+import { POScreen } from './components/POScreen';
+import { CSCScreen } from './components/CSCScreen';
+import { MMICScreen } from './components/MMICScreen';
 
 
 
@@ -30,12 +35,23 @@ function App() {
         headerTintColor: '#fff',
         headerTitleStyle: {
           fontWeight: 'bold',
+          color:'white',
         },
       }}
       initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} 
+        <Stack.Screen name="Home" component={HomeScreen}
         options={{ headerTitle: props => <LogoTitle {...props} /> }}/>
-        <Stack.Screen name="Details" component={DetailsScreen} 
+        <Stack.Screen name="ATMs" component={ATMScreen}
+        options={{ headerTitle: props => <LogoTitle {...props} /> }} />
+        <Stack.Screen name="Banks" component={BankScreen}
+        options={{ headerTitle: props => <LogoTitle {...props} /> }} />
+        <Stack.Screen name="BMs" component={BMScreen}
+        options={{ headerTitle: props => <LogoTitle {...props} /> }} />
+        <Stack.Screen name="POs" component={POScreen}
+        options={{ headerTitle: props => <LogoTitle {...props} /> }} />
+        <Stack.Screen name="CSCs" component={CSCScreen}
+        options={{ headerTitle: props => <LogoTitle {...props} /> }} />
+        <Stack.Screen name="MMIC" component={MMICScreen}
         options={{ headerTitle: props => <LogoTitle {...props} /> }} />
       </Stack.Navigator>
     </NavigationContainer>
